5f4ee698df92fae20d2275e2d2cc0fa4ba5a3f56
Fred Fu <moonsolo@gmail.com>
2020-11-04 14:59:14 -0500

    WIP FOL Engine

--- main2.rkt
+++ main2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(struct variable (name idx) #:transparent)
+(struct variable (name) #:transparent)
 
 (struct formula () #:transparent)
 (struct atom formula (name ts))
@@ -21,15 +21,18 @@
 
 
 (struct sentence (np vp) #:transparent)
-(struct np+ (kind det n)) ;; if kind = 1 n is a CN; otherwise n is a RCN
+(struct np+ () #:transparent)
+(struct np1 np+ (det cn))
+(struct np2 np+ (det rcn))
+
 (define (np? x)
   (or
-   (ormap (λ (a) (equal? x a)) (list 'Snowhite 'Alice 'Dorothy 'Godilocks 'LittleMock 'Atreyu))
+   (ormap (λ (a) (equal? x a)) (list 'Snowhite 'Alice 'JonSnow))
    (np+? x)))
 
 
 (define (det? x)
-  (ormap (curry equal? x) (list 'The 'Every 'Some 'No 'Most)))
+  (ormap (curry equal? x) (list 'Every)))
 
 (define (cn? x)
   (ormap (curry equal? x) (list 'Girl 'Boy 'Princess 'Dwarf 'Giant 'Wizard 'Sward 'Dagger)))
@@ -48,3 +51,27 @@
 (struct tv () #:transparent)
 (struct saw tv (np) #:transparent)
 
+
+;; LF-sentence : (-> sentence LF)
+(define (LF-sentence sent)
+  (LF-np (sentence-np sent) (sentence-vp sent)))
+
+;; LF-np : (-> np (-> (-> term LF) LF))
+(define (LF-np np)
+  (match np
+    ['Snowhite (λ (p) (p (struct-term "SnowWhite" [])))]
+    ['Alice (λ (p) (p (struct-term "Alice" [])))]
+    ['JonSnow (λ (p) (p (struct-term "JonSnow" [])))]
+    [(np1 det cn) ((LF-det det) (LF-cn cn))]
+    [(np2 det rcn) ((LF-det det) (LF-rcn rcn))]))
+
+;; LF-det : (-> Det (-> Term LF) (-> (-> Term LF) LF))
+(define (LF-det x p)
+  
+  (cond
+    [(equal? x 'Every)
+     (λ (q)
+       (define v (Variable (gensym 'x)))
+       (forall v
+               (impl (p (var-term v)))
+               (impl (q (var-term v)))))]))
